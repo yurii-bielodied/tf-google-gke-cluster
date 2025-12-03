7c976df78a300c59746462cc0013a826baba5a8f
yurii-bielodied <ybeloded@gmail.com>
2025-12-03 12:11:49 +0200

    Fix sentitive value issue

--- main.tf
+++ main.tf
@@ -3,18 +3,18 @@ provider "google" {
   # The GCP project to use
   project = var.GOOGLE_PROJECT
   # The GCP region to deploy resources in
-  region  = var.GOOGLE_REGION
+  region = var.GOOGLE_REGION
 }
 
 # Create the GKE (Google Kubernetes Engine) cluster
 resource "google_container_cluster" "this" {
   # Name of the cluster
-  name     = var.GKE_CLUSTER_NAME
+  name = var.GKE_CLUSTER_NAME
   # Location (region) for the cluster
   location = var.GOOGLE_REGION
 
   # Set initial node count (required, but will remove default pool)
-  initial_node_count       = 1
+  initial_node_count = 1
   # Remove default node pool to use custom node pools instead
   remove_default_node_pool = true
 
@@ -34,13 +34,13 @@ resource "google_container_cluster" "this" {
 # Create a custom node pool for the GKE cluster
 resource "google_container_node_pool" "this" {
   # Name of the node pool
-  name       = var.GKE_POOL_NAME
+  name = var.GKE_POOL_NAME
   # GCP project to use (derived from the cluster)
-  project    = google_container_cluster.this.project
+  project = google_container_cluster.this.project
   # Attach node pool to the created cluster
-  cluster    = google_container_cluster.this.name
+  cluster = google_container_cluster.this.name
   # Location (region)
-  location   = google_container_cluster.this.location
+  location = google_container_cluster.this.location
   # Number of nodes in the pool
   node_count = var.GKE_NUM_NODES
 
@@ -57,8 +57,8 @@ module "gke_auth" {
     google_container_cluster.this
   ]
   # Source of the module (Terraform Registry)
-  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  version      = ">= 24.0.0"
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version = ">= 24.0.0"
   # Project and cluster details for authentication
   project_id   = var.GOOGLE_PROJECT
   cluster_name = google_container_cluster.this.name
@@ -71,7 +71,7 @@ data "google_client_config" "current" {}
 # Data source to fetch details about the created GKE cluster
 data "google_container_cluster" "main" {
   # Name of the cluster
-  name     = google_container_cluster.this.name
+  name = google_container_cluster.this.name
   # Location (region)
   location = var.GOOGLE_REGION
 }
