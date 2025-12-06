e0eca2dc9c8de75b472562acbcab1a7e6bf3fdfd
yurii-bielodied <ybeloded@gmail.com>
2025-12-06 13:46:47 +0200

    Set initial_node_count to 1

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "google_container_cluster" "this" {
   location = var.GOOGLE_REGION
 
   # Set initial node count (required, but will remove default pool)
-  initial_node_count = 2
+  initial_node_count = 1
   # Remove default node pool to use custom node pools instead
   remove_default_node_pool = true
 
